78c1e87d6135abf0b8b9ccb9862c9cd58a7b666b
Sabrina <90803080+sabrinafn@users.noreply.github.com>
2025-12-29 22:13:38 -0300

    feat: implement custom user model from Django's AbstractUser

--- core/auth_service/auth/views.py
+++ core/auth_service/auth/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 def index(request):
     return HttpResponse("Hello, world. You're at the auth index.")
+
+def login():
+    """ create login endpoint"""
+    pass
+
+def register():
+    """ create register endpoint"""
+    pass
