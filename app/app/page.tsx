6c15f199a32122863374061078463a94975bf4b0
Juliany Bernardo <julianybernardo@outlook.com>
2026-01-11 01:07:02 -0300

    (feat) Dropdown menu layout

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -4,8 +4,8 @@ export default function LandingPage() {
 	return (
 		<>
 			<div className="min-h-screen flex flex-col items-center justify-center">
-				<p className="text-[200px] mb-4">ft_pong</p>
-				<Link href="/login" className="text-4xl">enter</Link>
+				<p className="text-[200px] text-amber-100 mb-4">ft_pong</p>
+				<Link href="/login" className="text-4xl text-amber-100">enter</Link>
 			</div>
 		</>
 	)
