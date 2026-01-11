d89c2718649d65edf77af98bfca79d92a5184ba9
Juliany Bernardo <julianybernardo@outlook.com>
2026-01-11 17:43:41 -0300

    (feat) Home design

--- app/app/home/page.tsx
+++ app/app/home/page.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 import Dropdown from "@/components/Dropdown";
 import FriendList from "@/components/Friends";
+import PersonalStats from "@/components/PersonalStats";
+import GeneralStats from "@/components/GeneralStats";
 
 export default function Home() {
   return (
     <>
       <div className="grid grid-cols-12 grid-rows-24 gap-2 h-dvh">
         <div className="row-span-1 pl-3 pt-1 pb-13 col-span-1">
-          <Link href="/profile">
+          <Link href="/home">
             <Image src="/home.svg" alt="home" className="pt-4 ml-5" width={31} height={31} />
           </Link>
           <br />
@@ -21,29 +23,50 @@ export default function Home() {
           <Dropdown />
         </div>
         <div className="row-span-23 row-start-3 col-span-10 grid grid-cols-subgrid gap-2">
-          <div className="row-span-11 pl-3 pt-1 col-span-5 bg-amber-400">
-            personal stats
+          <div className="row-span-8 col-span-5 mt-1">
+            <PersonalStats />
           </div>
-          <div className="row-span-11 pl-3 pt-1 col-span-5 bg-amber-400">
-            general stats
+
+          <div className="row-span-8 col-span-5 mt-1">
+            <GeneralStats />
           </div>
-          <div className="row-span-7 pl-3 pt-1 mt-8 mb-8 col-span-2 bg-amber-400">
-            <Link href="/chat">chat</Link>
-            <br />
-          </div>
-          <div className="row-span-7 pl-3 pt-1 mt-8 mb-8 col-span-2 bg-amber-400">
-            <Link href="/lobby">lobby</Link>
-          </div>
-          <div className="row-span-7 pl-3 pt-1 mt-8 mb-8 col-span-2 bg-amber-400">
-            new game
-          </div>
-          <div className="row-span-7 pl-3 pt-1 mt-8 mb-8 col-span-2 bg-amber-400">
-            tournaments
-          </div>
-          <div className="row-span-7 pl-3 pt-1 mt-8 mb-8 col-span-2 bg-amber-400">
-            find new friends
-          </div>
+
+          <Link href="/chat" className="row-span-1 row-start-10 col-span-2">
+            <div className="flex flex-col items-center">
+              <Image src="/chat.svg" alt="conversations" width={100} height={100} />
+              <p className="text-3xl mt-3 text-amber-100">conversations</p>
+            </div>
+          </Link>
+
+          <Link href="/lobby" className="row-span-1 row-start-10 col-span-2">
+            <div className="flex flex-col items-center">
+              <Image src="/lobby.svg" alt="lobby" width={100} height={100} />
+              <p className="text-3xl mt-3 text-amber-100">lobby</p>
+            </div>
+          </Link>
+
+          <Link href="/matchmaking" className="row-span-1 row-start-10 col-span-2">
+            <div className="flex flex-col items-center">
+              <Image src="/matchmaking.svg" alt="matchmaking" width={100} height={100} />
+              <p className="text-3xl mt-3 text-amber-100">matchmaking</p>
+            </div>
+          </Link>
+
+          <Link href="/tournament" className="row-span-1 row-start-10 col-span-2">
+            <div className="flex flex-col items-center">
+              <Image src="/tournament.svg" alt="tournament" width={100} height={100} />
+              <p className="text-3xl mt-3 text-amber-100">tournament</p>
+            </div>
+          </Link>
+
+          <Link href="/search" className="row-span-1 row-start-10 col-span-2">
+            <div className="flex flex-col items-center">
+              <Image src="/search.svg" alt="find new friends" width={100} height={100} />
+              <p className="text-3xl mt-3 text-amber-100">find new friends</p>
+            </div>
+          </Link>
         </div>
+
         <div className="row-span-22 row-start-3 pt-1 mr-2 mb-6 col-span-2 col-start-11">
           <FriendList />
         </div>
---
+++ app/components/PersonalStats.tsx
@@ -0,0 +1,7 @@
+export default function PersonalStats() {
+  return (
+    <div className="w-full h-full border-4 border-black bg-black/70">
+      <p className="text-amber-200 text-4xl text-center mt-8 mb-1">My stats</p>
+    </div>
+  )
+}
