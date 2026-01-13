b1eee2c9222f7fc993993f4814cad3afe54c67cf
Sabrina <90803080+sabrinafn@users.noreply.github.com>
2026-01-13 11:37:32 -0300

    fix: update tests for user authentication

--- core/accounts_service/accounts/tests/test_user_authentication.py
+++ core/accounts_service/accounts/tests/test_user_authentication.py
@@ -36,9 +36,9 @@ class AuthenticationTestCase(TestCase):
         self.assertIn('user', response.data)
         self.assertEqual(response.data['user']['email'], "user2@example.com")
 
-    def test_valid_user_login(self):
+    def test_valid_password_login(self):
         """
-        Test that registerd user can login successfully
+        Test that registered user can login successfully
         """
         data = {
             "email": "user@example.com",
@@ -51,9 +51,9 @@ class AuthenticationTestCase(TestCase):
         self.assertIn('user', response.data)
         self.assertEqual(response.data['user']['email'], "user@example.com")
 
-    def test_invalid_user_login(self):
+    def test_invalid_password_login(self):
         """
-        Test that user not registerd cannot login
+        Test that user not registered cannot login
         """
         data = {
             "email": "user@example.com",
@@ -63,3 +63,64 @@ class AuthenticationTestCase(TestCase):
         
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertNotIn('token', response.data)
+
+    def test_token_is_valid(self):
+        """
+        Test that with a valid token it is possible to see the profile.
+        """
+        data = {
+            "email": "user@example.com",
+            "password": "SecurePass123!"
+        }
+        login_response = self.client.post(self.login_url, data, format='json')
+        token = login_response.data["token"]
+
+        profile_response = self.client.get(self.profile_url, HTTP_AUTHORIZATION=f'Token {token}')
+
+        self.assertEqual(profile_response.status_code, status.HTTP_200_OK)
+        self.assertIn('id', profile_response.data)
+        self.assertIn('email', profile_response.data)
+        self.assertIn('username', profile_response.data)
+        self.assertEqual(profile_response.data['email'], "user@example.com")
+
+    def test_token_is_invalid(self):
+        """
+        Test that with a invalid token it is not possible to see the profile.
+        """
+        token = "invalidtoken"
+
+        profile_response = self.client.get(self.profile_url, HTTP_AUTHORIZATION=f'Token {token}')
+
+        self.assertEqual(profile_response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('id', profile_response.data)
+        self.assertNotIn('email', profile_response.data)
+        self.assertNotIn('username', profile_response.data)
+
+    def test_user_logout(self):
+        """
+        Test that a logged-in user can logout successfully and invalidate the token
+        """
+        data = {
+            "email": "user@example.com",
+            "password": "SecurePass123!"
+        }
+        login_response = self.client.post(self.login_url, data, format='json')
+        token = login_response.data["token"]
+
+        logout_response = self.client.post(self.logout_url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(logout_response.status_code, status.HTTP_200_OK)
+
+        profile_response = self.client.get(self.profile_url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(profile_response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertNotIn('id', profile_response.data)
+        self.assertNotIn('email', profile_response.data)
+        self.assertNotIn('username', profile_response.data)
+
+    def test_logout_without_token(self):
+        """
+        Test logout with invalid token
+        """
+        token = "invalidtoken"
+
+        logout_response = self.client.post(self.logout_url, HTTP_AUTHORIZATION=f'Token {token}')
+        self.assertEqual(logout_response.status_code, status.HTTP_401_UNAUTHORIZED)
